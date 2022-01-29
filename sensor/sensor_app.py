ab0267d266fb06f2e4ba53887f100e4e593f61fe
shiroziro <bongos-vibrato0i@icloud.com>
2022-01-29 16:26:22 +0900

    Module2 ended.

--- sensor/sensor_app.py
+++ sensor/sensor_app.py
@@ -1,5 +1,5 @@
 # Runner script for all modules
-
+from load_data import load_sensor_data
 
 ##############################
 # Do not remove these two lines
@@ -9,8 +9,11 @@ print("Sensor Data App")
 ##############################
 
 # Module 1 code here:
+# This module do nothing. Did you try?
 
 # Module 2 code here:
+data = load_sensor_data()
+print("Loaded records: {}".format(len(data)))
 
 # Module 3 code here:
 
